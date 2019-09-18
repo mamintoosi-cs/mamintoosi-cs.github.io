1fb709cf6d4ff1a900d148e6761e97783032cd2a
mamintoosi <m.amintoosi@gmail.com>
2019-09-19 01:37:40 +0430

    Replacing old version of reveal with the new one 3.8.0

--- slides/lib/js/reveal-init.js
+++
@@ -1,6 +0,0 @@
- Reveal.initialize({
-    dependencies: [    
-        { src: '../../plugin/external.js', condition: function() { return !!document.querySelector( '[data-external]' ); } }
-    ],
-    rtl: true
-});
