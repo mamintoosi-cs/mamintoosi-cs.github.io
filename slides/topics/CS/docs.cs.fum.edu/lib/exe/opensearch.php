b1f157ffaf273b06eaeca4ff54dd07e93b8f675c
Mahmood Amintoosi <mamintoosi@users.noreply.github.com>
2023-02-13 13:44:07 +0330

    Edit CS

---
+++ slides/topics/CS/docs.cs.fum.edu/lib/exe/opensearch.php
@@ -0,0 +1,7 @@
+<?xml version="1.0"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+  <ShortName> دانشکده علوم ریاضی </ShortName>
+  <Image width="16" height="16" type="image/x-icon">https://docs.ce.sharif.edu/lib/tpl/docs/images/favicon.ico</Image>
+  <Url type="text/html" template="https://docs.ce.sharif.edu/doku.php?do=search&amp;id={searchTerms}" />
+  <Url type="application/x-suggestions+json" template="https://docs.ce.sharif.edu/lib/exe/ajax.php?call=suggestions&amp;q={searchTerms}" />
+</OpenSearchDescription>
